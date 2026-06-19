4e8fc771270a17eb697cd5a714eb54f2d1bd5114
Brandon <145945720+Branwilli@users.noreply.github.com>
2026-06-19 13:46:44 -0500

    Add files via upload

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import waston_nlp
+import json 
+
+def emotion_detection(text_to_analyze):
+    emotional_model = waston_nlp.load('emotion-en')
+
+    response = emotional_model.run(text_to_analyze)
+    dominant_emotion = response['emotion']['predictions'][0]['label']
+    response['detected_emotion'] = dominant_emotion
+
+    response_json = json.dumps(response, indent=4)
+    response_dict = json.loads(response_json)
+
+    return response_dict
+
+if __name__ == "__main__":
+    sample_text = "I love this new technology."
+    sample_text = 'I am so happy I am doing this.'
+    result = emotion_detection(sample_text)
+    print("Detected Emotion:", result['detected_emotion'])
